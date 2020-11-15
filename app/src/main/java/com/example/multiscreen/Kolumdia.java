ad0f0dededb8c3c332c9598738bc1d7c07edbb97
Guslev Ivan <guslev55@gmail.com>
2020-11-15 16:15:16 +0900

    Гуслев Иван Приложение из нескольких активностей. ListView

---
+++ app/src/main/java/com/example/multiscreen/Kolumdia.java
@@ -0,0 +1,14 @@
+package com.example.multiscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kolumdia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kolumdia);
+    }
+}
